f2939a12e04316233ed69217e392eed7f2a97df8
cbmiller2610 <cbmiller2610@live.com>
2023-12-15 01:26:09 -0500

    still diverging

--- Euler_wrapper.py
+++ Euler_wrapper.py
@@ -17,20 +17,22 @@ def grid_transform(xi, eta, m1, m2, nozzle_xloc, nozzle_ymax, throat_xloc, throa
     return y
 
 def initial_cond(xi):
-    if (xi >= 0) & (xi < 0.5):
-        rho = 1.0
-        T = 1.0
-    elif (xi >= 0.5) & (xi < 1.5):
-        rho = 1.0 - 0.366 * (xi - 0.5)
-        T = 1.0 - 0.167 * (xi - 0.5)
-    elif (xi >= 1.5) & (xi < 2.1):
-        rho = 0.634 - 0.702 * (xi - 1.5)
-        T = 0.833 - 0.4908 * (xi - 1.5)
-    elif (xi >= 2.1) & (xi <= 3.0):
-        rho = 0.5892 + 0.10228 * (xi - 2.1)
-        T = 0.93968 + 0.0622 * (xi - 2.1) 
-    else:
-        print("OUT OF RANGE")
+    #if (xi >= 0) & (xi < 0.5):
+    #    rho = 1.0
+    #    T = 1.0
+    #elif (xi >= 0.5) & (xi < 1.5):
+    #    rho = 1.0 - 0.366 * (xi - 0.5)
+    #    T = 1.0 - 0.167 * (xi - 0.5)
+    #elif (xi >= 1.5) & (xi < 2.1):
+    #    rho = 0.634 - 0.702 * (xi - 1.5)
+    #    T = 0.833 - 0.4908 * (xi - 1.5)
+    #elif (xi >= 2.1) & (xi <= 3.0):
+    #    rho = 0.5892 + 0.10228 * (xi - 2.1)
+    #    T = 0.93968 + 0.0622 * (xi - 2.1) 
+    #else:
+    #    print("OUT OF RANGE")
+    rho = 1 
+    T = 1
     return rho, T
 
 def U_to_prim(U, xi_div, eta_div):
@@ -100,6 +102,52 @@ def Ughost_to_Fghost(U, xi_div):
     F_step[:, :, 3] = (g * U3 * U4) / U1 - ((g * (g - 1)) / 2) * ((U2**2 * U3 + U3**3) / U1)
     return F_step
 
+def grid_transform(xi, eta, m1, m2, nozzle_xloc,nozzle_ymax,throat_xloc,throat_ymax):
+
+
+    #Region 1: converging
+    if xi<=1.5:
+        y=eta * (m1 * (xi - inlet_xloc) + inlet_ymax)
+    #Region 2: diverging
+    elif (xi > 1.5):
+        y=eta * (m2 * (xi-throat_xloc) + throat_ymax)
+    #Region 3: farfield
+    # elif (xi > 5) & (xi <= 25):
+    #     y=eta*(m3*(xi-nozzle_xloc)+nozzle_ymax)
+    else:
+        print("WRONG VALUE")
+    return y
+
+def coord_transform(U_np_3D, E_np_3D, F_np_3D, xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax):
+    if xi_grid <= 1.5:
+        ys = (m1 * (xi_grid - inlet_xloc) + inlet_ymax)
+        m=m1
+    elif xi_grid > 1.5:
+        ys = (m2 * (xi_grid - throat_xloc) + throat_ymax)
+        m=m2
+    else:
+        print("WRONG VALUE")
+    U_new = U_np_3D * ys
+    E_new = E_np_3D * ys
+    F_new = -m * eta_grid * E_np_3D + F_np_3D
+    return U_new, E_new, F_new
+
+def return_transform(U_np_3D, E_np_3D, F_np_3D, xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax):
+    if xi_grid <= 1.5:
+        ys = (m1 * (xi_grid - inlet_xloc) + inlet_ymax)
+        m=m1
+    elif xi_grid > 1.5:
+        ys = (m2 * (xi_grid - throat_xloc) + throat_ymax)
+        m=m2
+    else:
+        print("WRONG VALUE")
+    U_new = U_np_3D / ys
+    E_new = E_np_3D / ys
+    F_new = F_np_3D + (m * eta_grid * E_np_3D) / ys
+    return U_new, E_new, F_new
+
+
+
 #Import C dlls
 #solver_dll = CDLL("./euler_solver.so")
 #solver_dll.argtypes = [c_double, c_double, c_double, c_int, c_int, c_int]
@@ -107,46 +155,42 @@ def Ughost_to_Fghost(U, xi_div):
 #Gridding Inputs
 xi_max = 3
 eta_max = 1
-xi_div = 201
-eta_div = 101
+xi_div = 1001
+eta_div = 501
 dxi = xi_max / (xi_div - 1)
 deta = eta_max / (eta_div - 1)
-dt = 0.0008
-tsteps = 1000
+dt = 0.001
+tsteps = 200
 
 #Grid Generation
+xi_vec = np.linspace(0,xi_max,xi_div)
+eta_vec = np.linspace(0,eta_max,eta_div)
 
-xi_vec = np.linspace(0, xi_max, xi_div)
-eta_vec = np.linspace(0, eta_max, eta_div)
-
-xi_grid,eta_grid = np.meshgrid(xi_vec, eta_vec, indexing='ij')
+xi_grid,eta_grid = np.meshgrid(xi_vec,eta_vec,indexing='ij')
 x_grid = xi_grid
 
 grid_trans_func = np.vectorize(grid_transform)
-throat_area = 1
-throat_diam = 2 * np.sqrt(throat_area / np.pi)
-throat_ymax = throat_diam / 2
-inlet_area = 5.95
-nozzle_area= 5.95
-inlet_diam = 2 * np.sqrt(inlet_area / np.pi)
-inlet_ymax = inlet_diam / 2
-nozzle_diam = 2 * np.sqrt(nozzle_area / np.pi)
-nozzle_ymax = nozzle_diam / 2
+throat_ymax = 1
+inlet_ymax = np.sqrt(5.95)
+nozzle_ymax = np.sqrt(5.95)
 
 throat_xloc = 1.5
 inlet_xloc = 0
 nozzle_xloc = 3
 
-m1 = (throat_ymax - inlet_ymax) / (throat_xloc - inlet_xloc)
-m2 = (nozzle_ymax - throat_ymax) / (nozzle_xloc - throat_xloc)
-y_grid = grid_trans_func(xi_grid, eta_grid, m1, m2, nozzle_xloc, nozzle_ymax, throat_xloc, throat_ymax)
+
+m1 = (throat_ymax - inlet_ymax)/(throat_xloc - inlet_xloc)
+m2 = (nozzle_ymax - throat_ymax)/(nozzle_xloc - throat_xloc)
+
+y_grid = grid_trans_func(xi_grid,eta_grid, m1, m2, nozzle_xloc,nozzle_ymax,throat_xloc,throat_ymax)
 
 #Initial Conditions
 
 IC_func = np.vectorize(initial_cond)
 rho, T = IC_func(xi_grid)
-u = 0.59 / rho
-v = np.zeros((xi_div, eta_div))
+#u = 0.59 / rho
+u = np.full((xi_div, eta_div),0)
+v = np.full((xi_div, eta_div), 0)
 g = 1.4
 
 #Creation of the Tensors of Conserved Variables and Fluxes as C Arrays
@@ -165,7 +209,12 @@ g = 1.4
 #F_np_1D = np.cytypeslib.as_array(F)
 #F_np_3D = np.reshape(F_np_1D, (xi_div, eta_div, 4))
 P_stor = np.empty((xi_div, eta_div, 5, tsteps+1)) #store rho, u, v, p, and T values
+U_stor = np.empty((xi_div, eta_div, 4))
+E_stor = np.empty((xi_div, eta_div, 4))
+F_stor = np.empty((xi_div, eta_div, 4))
 U_np_3D = np.empty((xi_div, eta_div, 4))
+Upred = np.empty((xi_div, eta_div, 4))
+Ucorr = np.empty((xi_div, eta_div, 4))
 E_np_3D = np.empty((xi_div, eta_div, 4))
 F_np_3D = np.empty((xi_div, eta_div, 4))
 
@@ -189,7 +238,25 @@ F_np_3D[:, :, 1] = (U2 * U3) / U1
 F_np_3D[:, :, 2] = U3**2 / U1 + (1 - 1 / g) * (U4 - (g / 2) * ((U2**2 + U3**2) / U1))
 F_np_3D[:, :, 3] = (g * U3 * U4) / U1 - ((g * (g - 1)) / 2) * ((U2**2 * U3 + U3**3) / U1)
 
+with open('TEST_U_before.dat', 'wb') as f:
+    np.save(f, U_np_3D)
+
 P_stor[:, :, :, 0] = U_to_prim(U_np_3D, xi_div, eta_div)
+#Transform from Cartesian to Computational Grid
+coord_transform_func = np.vectorize(coord_transform)
+return_transform_func = np.vectorize(return_transform)
+U_np_3D[:, :, 0], E_np_3D[:, :, 0], F_np_3D[:, :, 0] = coord_transform_func(U_np_3D[:, :, 0], E_np_3D[:, :, 0], F_np_3D[:, :, 0], xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax)
+U_np_3D[:, :, 1], E_np_3D[:, :, 1], F_np_3D[:, :, 1] = coord_transform_func(U_np_3D[:, :, 1], E_np_3D[:, :, 1], F_np_3D[:, :, 1], xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax)
+U_np_3D[:, :, 2], E_np_3D[:, :, 2], F_np_3D[:, :, 2] = coord_transform_func(U_np_3D[:, :, 2], E_np_3D[:, :, 2], F_np_3D[:, :, 2], xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax)
+U_np_3D[:, :, 3], E_np_3D[:, :, 3], F_np_3D[:, :, 3] = coord_transform_func(U_np_3D[:, :, 3], E_np_3D[:, :, 3], F_np_3D[:, :, 3], xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax)
+
+U_inlet = U_to_prim(U_np_3D, xi_div, eta_div)
+T_inlet = U_inlet[0, 1, 4]
+rho_inlet = U_inlet[0,1,0]
+
+with open('TEST_U_after.dat', 'wb') as f:
+    np.save(f, U_np_3D)
+
 Ughosts = np.empty((xi_div-2, 2, 4))
 Eghosts = np.empty((xi_div-2, 2, 4))
 Fghosts = np.empty((xi_div-2, 2, 4))
@@ -197,26 +264,22 @@ Fghosts = np.empty((xi_div-2, 2, 4))
 for i in range(1,tsteps):
     #Update boundary nodes from last timestep
     #left boundary, subsonic inlet (2 float, 2 prescribed)
-    T = 1
-    rho = 1
-    U_np_3D[0, :, 0] = rho
+    U_np_3D[0, :, 0] = rho_inlet
     U_np_3D[0, :, 1:3] = 2 * U_np_3D[1, :, 1:3] - U_np_3D[2, :, 1:3]
     u0 = U_np_3D[0, :, 1] / U_np_3D[0, :, 0]
     v0 = U_np_3D[0, :, 2] / U_np_3D[0, :, 0]
-    #U_prim = U_to_prim(U_np_3D, xi_div, eta_div)
-    U_np_3D[0, :, 3] = rho * (T / (g - 1) + (g / 2) * (u0**2 + v0**2))
+    U_np_3D[0, :, 3] = rho_inlet * (T_inlet / (g - 1) + (g / 2) * (u0**2 + v0**2))
 
     #right boundary, supersonic outlet, all quantities float
     U_np_3D[-1, :, :] = 2 * U_np_3D[-2, :, :] - U_np_3D[-3, :, :]
-    U_init = U_np_3D
     #Wall and symmetry boundaries (update ghosts)
     #Lower Boundary
-    Ughosts[:, 0, :] = U_np_3D[1:-1, 0, :]
-    Ughosts[:, 0, 2] = - U_np_3D[1:-1, 0, 2]
+    Ughosts[:, 0, :] = U_np_3D[1:-1, 1, :]
+    Ughosts[:, 0, 2] = - U_np_3D[1:-1, 1, 2]
     #Upper Boundary
-    Ughosts[:, 1, :] = U_np_3D[1:-1, -1, :]
-    Ughosts[:, 1, 2] = - U_np_3D[1:-1, -1, 2]
-    
+    Ughosts[:, 1, :] = U_np_3D[1:-1, -2, :]
+    Ughosts[:, 1, 2] = - U_np_3D[1:-1, -2, 2]
+
     #Update Flux Terms
     E_np_3D = U_to_E(U_np_3D, xi_div, eta_div)
     F_np_3D = U_to_F(U_np_3D, xi_div, eta_div)
@@ -224,36 +287,48 @@ for i in range(1,tsteps):
     Fghosts = Ughost_to_Fghost(Ughosts, xi_div)
 
     #Predictor Step (Upper boundary uses ghost)
-   #Update Upper Before Internal Flow
-    U_np_3D[1:-1, -1, :]  = (U_np_3D[1:-1, -1, :] - (dt / dxi) * (E_np_3D[2:xi_div, -1, :] - E_np_3D[1:-1, -1, :])
+    #Update Upper Before Internal Flow
+    Upred = U_np_3D
+    Upred[1:-1, -1, :]  = (U_np_3D[1:-1, -1, :]
+                            - (dt / dxi) * (E_np_3D[2:xi_div, -1, :] - E_np_3D[1:-1, -1, :])
                             - (dt / deta) * (Fghosts[:, 1, :] - F_np_3D[1:-1, -1, :]))
     #Inner
-    U_np_3D[1:-1, 0:-1, :] = (U_np_3D[1:-1, 0:-1, :]
+    Upred[1:-1, 0:-1, :] = (U_np_3D[1:-1, 0:-1, :]
                             - (dt / dxi) * (E_np_3D[2:xi_div, 0:-1, :] - E_np_3D[1:-1, 0:-1, :])
                             - (dt / deta) * (F_np_3D[1:-1, 1:eta_div, :] - F_np_3D[1:-1, 0:-1, :]))
 
     #Corrector Step (Lower boundary uses ghost)
-    #Update Flux Terms to Next Time Level
-    E_np_3D = U_to_E(U_np_3D, xi_div, eta_div)
-    F_np_3D = U_to_F(U_np_3D, xi_div, eta_div)
+    #Update Flux Terms from Predictor
+    E_np_3D = U_to_E(Upred, xi_div, eta_div)
+    F_np_3D = U_to_F(Upred, xi_div, eta_div)
     #Update Lower Before Internal Flow
-    U_np_3D[1:-1, 0, :]  = 0.5 * (U_np_3D[1:-1, 0, :] + U_init[1:-1, 0, :]
+    Ucorr = U_np_3D
+    Ucorr[1:-1, 0, :]  = (U_np_3D[1:-1, 0, :]
                             - (dt / dxi) * (E_np_3D[1:-1, 0, :] - E_np_3D[0:-2, 0, :])
                             - (dt / deta) * (F_np_3D[1:-1, 0, :] - Fghosts[:, 0, :]))
     #Inner
-    U_np_3D[1:-1, 1:eta_div, :] = 0.5 * (U_np_3D[1:-1, 1:eta_div, :]  + U_init[1:-1, 1:eta_div, :]
-                                         - (dt / dxi) * (E_np_3D[1:-1, 1:eta_div, :] - E_np_3D[0:-2, 1:eta_div, :])
-                                         - (dt / deta) * (F_np_3D[1:-1, 1:eta_div, :] - F_np_3D[1:-1, 0:-1, :]))
+    Ucorr[1:-1, 1:eta_div, :] = (U_np_3D[1:-1, 1:eta_div, :]
+                                 - (dt / dxi) * (E_np_3D[1:-1, 1:eta_div, :] - E_np_3D[0:-2, 1:eta_div, :])
+                                 - (dt / deta) * (F_np_3D[1:-1, 1:eta_div, :] - F_np_3D[1:-1, 0:-1, :]))
+
+    #Updator
+    U_np_3D = 0.5 * (Upred + Ucorr)
+
+    #Update Flux Terms
+    E_np_3D = U_to_E(U_np_3D, xi_div, eta_div)
+    F_np_3D = U_to_F(U_np_3D, xi_div, eta_div)
     
     #Transform Back to Physical Coordinates
-    xi_max = 3
-    eta_max = 1
-    xi_vec = np.linspace(0,xi_max,xi_div)
-    eta_vec = np.linspace(0,eta_max,eta_div)
-    xi_grid,eta_grid = np.meshgrid(xi_vec,eta_vec,indexing='ij')
+
+    U_stor[:, :, 0], E_stor[:, :, 0], F_stor[:, :, 0] = return_transform_func(U_np_3D[:, :, 0], E_np_3D[:, :, 0], F_np_3D[:, :, 0], xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax)
+    U_stor[:, :, 1], E_stor[:, :, 1], F_stor[:, :, 1] = return_transform_func(U_np_3D[:, :, 1], E_np_3D[:, :, 1], F_np_3D[:, :, 1], xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax)
+    U_stor[:, :, 2], E_stor[:, :, 2], F_stor[:, :, 2] = return_transform_func(U_np_3D[:, :, 2], E_np_3D[:, :, 2], F_np_3D[:, :, 2], xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax)
+    U_stor[:, :, 3], E_stor[:, :, 3], F_stor[:, :, 3] = return_transform_func(U_np_3D[:, :, 3], E_np_3D[:, :, 3], F_np_3D[:, :, 3], xi_grid, eta_grid, m1, m2, inlet_xloc, inlet_ymax, throat_xloc, throat_ymax)
 
 
-    P_stor[:, :, :, i] = U_to_prim(U_np_3D, xi_div, eta_div)
+    P_stor[:, :, :, i] = U_to_prim(U_stor, xi_div, eta_div)
 
 with open('TEST.dat', 'wb') as f: 
     np.save(f, P_stor)
+#with open('ustor.dat','wb') as f:
+#    np.save(f, U_stor)
